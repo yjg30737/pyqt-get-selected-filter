ddf67f6c58e3cafa4ec136b69f3c35f48a6d0e07
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 07:08:40 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-get-selected-filter',
@@ -8,5 +15,7 @@ setup(
     license='MIT',
     packages=find_packages(),
     description='Get selected filter string for QFileDialog',
-    url='https://github.com/yjg30737/pyqt-get-selected-filter.git'
+    url='https://github.com/yjg30737/pyqt-get-selected-filter.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description
 )
